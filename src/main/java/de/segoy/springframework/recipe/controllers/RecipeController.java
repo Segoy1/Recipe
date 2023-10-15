e14e03223dbe533743fdae485a9b5f7d7c44e93d
Segoy <segoy.thiel@protonmail.com>
2023-10-15 15:32:46 +0200

    added Form to add new recipe

--- src/main/java/de/segoy/springframework/recipe/controllers/RecipeController.java
+++ src/main/java/de/segoy/springframework/recipe/controllers/RecipeController.java
@@ -1,11 +1,11 @@
 package de.segoy.springframework.recipe.controllers;
 
 
+import de.segoy.springframework.recipe.commands.RecipeCommand;
 import de.segoy.springframework.recipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RecipeController {
@@ -21,5 +21,19 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(Long.parseLong(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+        return "redirect/recipe/show/" + savedCommand.getId();
     }
 }
